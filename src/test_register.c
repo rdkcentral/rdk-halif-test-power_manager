ecf26fc8c1e81007e824cc1323de68f8822b57cb
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2024-09-27 11:15:39 -0400

    gh #22 powerManager: L3 Test case Development

--- src/test_register.c
+++ src/test_register.c
@@ -77,31 +77,13 @@ extern int test_l2_plat_power_register( void );
 /*L3 Testing Functions*/
 extern int test_l3_plat_power_register( void );
 
-int UT_register_APIDEF_l1_tests( void )
+int UT_register_tests( void )
 {
 	int registerFailed=0;
 
 	registerFailed |= test_l1_plat_power_register();
-
-	return registerFailed;
-}
-
-/* Register UT Functions */
-int UT_register_APIDEF_l2_tests( void )
-{
-	int registerFailed=0;
-
-	registerFailed |= test_l2_plat_power_register();
-
-	return registerFailed;
-}
-
-/* Register UT Functions */
-int UT_register_APIDEF_l3_tests( void )
-{
-	int registerFailed=0;
-
-	registerFailed |= test_l3_plat_power_register();
+    registerFailed |= test_l2_plat_power_register();
+    registerFailed |= test_l3_plat_power_register();
 
 	return registerFailed;
 }
